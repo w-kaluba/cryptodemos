63ca8f7f36b9bc5fcce8b025e433f1ab65600979
Wiza Kaluba <kaluba.w@gmail.com>
2020-01-08 23:16:10 +0000

    Wrote prototype for the diffie hellman algorithm

---
+++ python/diffiehellman.py
@@ -0,0 +1,11 @@
+"""
+The Diffie-Hellman key exchange is a process that allows users to exchange a secret key
+to use in a symmetric key encryption system. The security of this problem is equivalent to solving the
+discrete logarithm problem.
+"""
+
+def generate_prime(N)
+"""
+Generates a prime of size at least N to use as a modulus for the DFH Key Exchange
+"""
+pass
